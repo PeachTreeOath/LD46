4c7c86f6b678e2489997d6571154a2341937fc08
Ian <russmoonbase@gmail.com>
2020-04-18 10:39:55 -0700

    Bullet Switching with mouse wheel or Q & E keys

--- Assets/Scripts/Cannon/CannonShoot.cs
+++ Assets/Scripts/Cannon/CannonShoot.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class CannonShoot : MonoBehaviour
 {
-   [SerializeField] private GameObject bulletPrefab;
+   [SerializeField] private GameObject[] bulletPrefabs;
    [SerializeField] private Transform cannonEnd;
    [SerializeField] private float shootingCooldown = 0.25f;
    [SerializeField] private float bulletShootForce = 5f;
+
+   private int selectedBulletIndex = 0;
    private float shootRate = 0.0f;
    private GameObject bullet;
+   private GameObject selectedBulletPrefab;
 
 
 
    // Start is called before the first frame update
    void Start()
    {
-
+      selectedBulletPrefab = bulletPrefabs[selectedBulletIndex];
    }
 
    // Update is called once per frame
@@ -31,13 +34,56 @@ public class CannonShoot : MonoBehaviour
       {
          Shoot();
       }
+
+      if (Input.GetAxis("Mouse ScrollWheel") > 0.0f || Input.GetKeyDown(KeyCode.E))
+      {
+         BulletSelectionForward();
+      }
+
+      if (Input.GetAxis("Mouse ScrollWheel") < 0.0f || Input.GetKeyDown(KeyCode.Q))
+      {
+         BulletSelectionBackward();
+      }
    }
 
    private void Shoot()
    {
-      bullet = Instantiate(bulletPrefab, cannonEnd.position, Quaternion.identity);
+      bullet = Instantiate(selectedBulletPrefab, cannonEnd.position, Quaternion.identity);
+ 
       bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletShootForce, ForceMode.Impulse);
 
       shootRate = shootingCooldown;
+   }
+
+   private void BulletSelectionForward()
+   {
+      if (selectedBulletIndex >= bulletPrefabs.Length - 1)
+      {
+         selectedBulletIndex = 0;
+         Debug.Log("Selected bullet number (forward) = " + selectedBulletIndex);
+      }
+      else
+      {
+         selectedBulletIndex++;
+         Debug.Log("Selected bullet number (forward) = " + selectedBulletIndex);
+      }
+
+      selectedBulletPrefab = bulletPrefabs[selectedBulletIndex];
+   }
+
+   private void BulletSelectionBackward()
+   {
+      if (selectedBulletIndex <= 0)
+      {
+         selectedBulletIndex = bulletPrefabs.Length - 1;
+         Debug.Log("Selected bullet number (backward) = " + selectedBulletIndex);
+      }
+      else
+      {
+         selectedBulletIndex--;
+         Debug.Log("Selected bullet number (backward) = " + selectedBulletIndex);
+      }
+
+      selectedBulletPrefab = bulletPrefabs[selectedBulletIndex];
    }
 }
