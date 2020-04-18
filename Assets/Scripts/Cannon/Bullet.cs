aae9fcce6d0a5683651b7760097b9f2e4abe4a78
Ian <russmoonbase@gmail.com>
2020-04-18 16:22:06 -0700

    Added initial stages of shooting two different bullet types (straight & lob)

--- Assets/Scripts/Cannon/Bullet.cs
+++ Assets/Scripts/Cannon/Bullet.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Inspector set
-    public FoodType foodType;
+   // Inspector set
+   public FoodType foodType;
+   public BulletType bulletType;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   // Start is called before the first frame update
+   void Start()
+   {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+
+   }
+
 }
