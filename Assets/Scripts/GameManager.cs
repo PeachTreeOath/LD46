5937e4c5b8262967f8995bbf91524e8f99d65aef
PeachTreeOath <peachtreeoath@gmail.com>
2020-04-18 00:38:08 -0600

    Spawning stuff that moves

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // Inspector set
+    public float landmarkSpawnTime;
+
+    private float landmarkSpawnTimeElapsed;
+    private List<GameObject> stationaryObjects = new List<GameObject>();
+
+    private const float moveSpeed = 5; // This moves all stationary objects. The cars themselves are still.
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Move all stationary objects
+        foreach (GameObject obj in stationaryObjects)
+        {
+            obj.transform.position += new Vector3(0, 0, moveSpeed * Time.deltaTime);
+        }
+
+        // Spawn logic
+        landmarkSpawnTimeElapsed += Time.deltaTime;
+
+        if (landmarkSpawnTimeElapsed > landmarkSpawnTime)
+        {
+            landmarkSpawnTimeElapsed -= landmarkSpawnTime;
+
+            SpawnLandmark();
+        }
+    }
+
+    private void SpawnLandmark()
+    {
+        GameObject landmarkObj = Instantiate(ResourceLoader.instance.testLandmarkPrefab);
+        landmarkObj.transform.position = new Vector3(UnityEngine.Random.Range(-10f, 10f), 0.5f, -50);
+        stationaryObjects.Add(landmarkObj);
     }
 }
---
+++ Assets/Scripts/ResourceLoader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceLoader : Singleton<ResourceLoader>
+{
+
+    [HideInInspector] public GameObject testLandmarkPrefab;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        LoadResources();
+    }
+
+    private void LoadResources()
+    {
+        testLandmarkPrefab = Resources.Load<GameObject>("Prefabs/testLandmark");
+    }
+}
