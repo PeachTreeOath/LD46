fe999416f3fe0df4bf69c2739982b8cfcbb69d9c
Ian <russmoonbase@gmail.com>
2020-04-18 12:24:49 -0700

    Added initial zoom in Aim Camera

--- Assets/Scripts/Camera/CameraLogic.cs
+++ Assets/Scripts/Camera/CameraLogic.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class CameraLogic : MonoBehaviour
 {
+   private const int HIGH_PRIORITY_NUM = 11;
+   private const int LOW_PRIORITY_NUM = 9;
+
+   private Cinemachine.CinemachineVirtualCameraBase virtualCamera;
+
    // Start is called before the first frame update
    void Start()
    {
-
+      virtualCamera = GetComponent<Cinemachine.CinemachineVirtualCameraBase>();
    }
 
    // Update is called once per frame
    void Update()
    {
+      if (Input.GetButtonDown("Fire2"))
+      {
+         ChangeCameraPriority();
+      }
+   }
 
+   private void ChangeCameraPriority()
+   {
+      if (virtualCamera)
+      {
+         if (virtualCamera.Priority == LOW_PRIORITY_NUM)
+         {
+            virtualCamera.Priority = HIGH_PRIORITY_NUM;
+         }
+         else if (virtualCamera.Priority == HIGH_PRIORITY_NUM)
+         {
+            virtualCamera.Priority = LOW_PRIORITY_NUM;
+         }
+      }
    }
 }
