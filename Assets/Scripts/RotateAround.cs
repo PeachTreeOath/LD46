bff60016501a0bc0b6eb286704477c7ef5ba88ec
Sean Kevin Farrell <seanykevinfarrell@gmail.com>
2020-04-18 19:14:37 -0600

    Materials for the sun/moon and a rotation script for them.

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(player.transform.position, Vector3.left, speed * Time.deltaTime);
+    }
+}
