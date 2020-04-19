465d83637b24bf71a8005773ee9f622da62128df
Sean Kevin Farrell <seanykevinfarrell@gmail.com>
2020-04-18 21:09:28 -0600

    Level world text & Text mesh pro integration

---
+++ Assets/SwitchSettingsOnInterval.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SwitchSettingsOnInterval : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float interval = 0.5f;
+    private bool boldEnable = false;
+    private float t;
+    private TextMeshProUGUI text;
+    void Start()
+    {
+        t = 0;
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(t > interval)
+        {
+            if (boldEnable)
+            {
+                text.fontStyle = FontStyles.Normal;
+                boldEnable = false;
+                t = 0;
+            }
+            else
+            {
+                text.fontStyle = FontStyles.Bold;
+                boldEnable = true;
+                t = 0;
+            }
+
+        }
+        else
+        {
+            t += Time.deltaTime;
+        }
+        
+    }
+}
