5dbe02f4a4e2b15030cf787484cab2ed5e5833f5
Sean Kevin Farrell <seanykevinfarrell@gmail.com>
2020-04-18 22:35:46 -0600

    Few scripts to manipulate the world canvas for level.

---
+++ Assets/DestroyUnit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyUnit : MonoBehaviour
+{
+    public float lifetime;
+    private float t = 0;
+    void Update()
+    {
+        if (t > lifetime)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            t += Time.deltaTime;
+        }    
+    }
+}
---
+++ Assets/SetTextTo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SetTextTo : MonoBehaviour
+{
+    public string defaultText;
+    private TextMeshProUGUI tmp;
+
+
+    private void Start()
+    {
+        tmp = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void SetTextToString(string text)
+    {
+        tmp.text = text;
+    }
+}
