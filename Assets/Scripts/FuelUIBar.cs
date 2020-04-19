61282c1103669080b038627eb4cf7eb977b2b5bc
Sean Kevin Farrell <seanykevinfarrell@gmail.com>
2020-04-19 15:15:57 -0600

    Start of the main ui

---
+++ Assets/Scripts/FuelUIBar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FuelUIBar : MonoBehaviour
+{
+    private Slider slider;
+    public float fillSpeed = 0;
+    private float targetProgress = 0;
+    private ParticleSystem ps;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+        ps = GameObject.Find("FuelParticles").GetComponent<ParticleSystem>();
+    }
+
+    void Start()
+    {
+        //IncrementProgress(0.75f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(slider.value < targetProgress)
+        {
+            slider.value += fillSpeed * Time.deltaTime;
+            if(!ps.isPlaying)
+            {
+                ps.Play();
+            }    
+        }
+        else
+        {
+            ps.Stop();
+        }
+    }
+
+    public void IncrementProgress(float newProgress)
+    {
+        targetProgress = slider.value + newProgress;
+    }
+}
---
+++ Assets/SwitchSettingsOnCondition.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SwitchSettingsOnCondition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float amount = 0.25f;
+    public float flick = 0.85f;
+    private float t = 0;
+    private bool boldEnable = false;
+    private TextMeshProUGUI text;
+    public Slider value;
+
+    void Start()
+    {
+        t = 0;
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (value.value < amount)
+        {
+            if (t > flick)
+            {
+                if (boldEnable)
+                {
+                    text.fontStyle = FontStyles.Italic;
+                    boldEnable = false;
+                    t = 0;
+                }
+                else
+                {
+                    text.fontStyle = FontStyles.Bold;
+                    text.fontStyle = FontStyles.Italic;
+                    boldEnable = true;
+                    t = 0;
+                }
+            }
+            else
+            {
+                t += Time.deltaTime;
+            }
+                
+
+        }
+
+    }
+}
